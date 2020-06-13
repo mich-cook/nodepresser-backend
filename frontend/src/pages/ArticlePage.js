fb2bf6fa765b906ec37dc3eb3c02a0b64b2367db
mich <mich-cook@users.noreply.github.com>
2020-06-13 16:39:38 -0700

    first pass at rebuilding the single article page to use mongodb store via the express API. for now upvotes and comments aren't enabled.

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,50 +1,51 @@
 import React, { useState, useEffect } from 'react';
-import ArticlesList from '../components/ArticlesList';
-import CommentsList from '../components/CommentsList';
-import UpvotesSection from '../components/UpvotesSection';
-import AddCommentForm from '../components/AddCommentForm';
+// import ArticlesList from '../components/ArticlesList';
+// import CommentsList from '../components/CommentsList';
+// import UpvotesSection from '../components/UpvotesSection';
+// import AddCommentForm from '../components/AddCommentForm';
 import NotFoundPage from '../pages/NotFoundPage';
-import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => {
-  const name = match.params.name;
-  const article = articleContent.find(article => article.name === name);
 
-  const [ articleInfo, setArticleInfo ] = useState({
-    "upvotes": 0,
+  const author = match.params.author;
+  const slug = match.params.slug;
+
+  const [ article, setArticle ] = useState({});
+/*    "upvotes": 0,
     "comments": []
-  });
+  }); */
 
   useEffect(() => {
     // since the function passed to useEffect can't be async
     const fetchData = async () => {
-      // const result = await fetch(`//localhost:8000/api/articles/${name}`);
-      const result = await fetch(`/api/articles/${name}`);
+      const result = await fetch(`//localhost:8000/api/article/${author}/${slug}`);
       const body = await result.json();
-      console.log(body);
-      setArticleInfo(body);
+      setArticle(body);
     }
     fetchData();
     // setArticleInfo({ "upvotes": Math.ceil(Math.random() * 10) });
+  }, [author, slug]);     // only do this when author or slug changes
 
-    // only do this when name (URL) changes
-  }, [name]);
-
-  if (!article) return <NotFoundPage />;
-  const otherArticles = articleContent.filter(article => article.name !== name);
+  if (article.status === "fail") return <NotFoundPage />;
+//  const otherArticles = articleContent.filter(article => article.name !== name);
 
   return(
   <React.Fragment>
     <h1>{article.title}</h1>
-    <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
+    <p>by {article.author}</p>
+    <p>{article.content}</p>
+  </React.Fragment>
+);}
+
+/*
     {article.content.map((paragraph, key) => (
       <p key={key}>{paragraph}</p>
     ))}
+    <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
     <CommentsList comments={articleInfo.comments} />
     <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
     <h3>Other Articles</h3>
     <ArticlesList articles={otherArticles} />
-  </React.Fragment>
-);}
+*/
 
 export default ArticlePage;
