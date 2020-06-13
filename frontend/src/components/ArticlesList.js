438274f67bbf3e10a5d49618c68d31ea3e31e845
mich <mich-cook@users.noreply.github.com>
2020-06-13 00:38:40 -0700

    connect the react frontend to the monogodb data store via the express-powered API

--- frontend/src/components/ArticlesList.js
+++ frontend/src/components/ArticlesList.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ArticlesList = ({ articles }) => (
   <React.Fragment>
       {articles.map((article, key) => (
-      <li key={key}><Link to={`/article/${article.name}`}>{article.title}</Link><p>{article.content[0].substring(0,150)}...</p></li>
+      <li key={key}><Link to={`/article/${article.slug}`}>{article.title}</Link><p>{article.content.substring(0,150)}...</p></li>
       ))}
   </React.Fragment>
 );
--- frontend/src/pages/ArticlesListPage.js
+++ frontend/src/pages/ArticlesListPage.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ArticlesList from '../components/ArticlesList';
-import articleContent from './article-content';
 
-const ArticlesListPage = () => (
-  <React.Fragment>
-    <h1>Articles</h1>
-    <ul>
-      <ArticlesList articles={articleContent} />
-    </ul>
-  </React.Fragment>
-);
+class ArticlesListPage extends Component {
+  constructor() {
+    super();
+    this.author = 'TestUser';  // not how we'll keep doing this, but sets it for now
+    this.state = {
+      articles: []
+    };
+  }
+
+  // TODO: Handle author doesn't exist or has been deactivated
+  componentDidMount() {
+    fetch(`http://localhost:8000/api/articles/${this.author}`)
+    .then(results => results.json())
+    .then(data => {
+      this.setState({ "articles": data });
+    });
+  }
+
+  // TODO: Better handling of author that doesn't exist or doesn't have articles
+  render() {
+    return (
+<React.Fragment>
+  <h1>Articles by {this.author}</h1>
+  <ul>
+    <ArticlesList articles={this.state.articles} />
+  </ul>
+</React.Fragment>
+    );
+  }
+};
 
 export default ArticlesListPage;
