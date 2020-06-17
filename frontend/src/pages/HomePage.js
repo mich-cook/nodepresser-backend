e9be19da2dd4ac6af67276b89a0b8098cb5724d7
mich <mich-cook@users.noreply.github.com>
2020-06-16 23:41:13 -0700

    better homepage content for multiple users

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const HomePage = () => (
   <React.Fragment>
-    <h1>Hello. Welcome to my blog!</h1>
-    <p>Jibber jabber</p>
-    <p>More Stuff</p>
+    <h1>Welcome to this blogging platform</h1>
+    <p>Top Authors (TODO):</p>
+    <ul></ul>
+    <p>Top Articles (TODO):</p>
+    <ul></ul>
   </React.Fragment>
 );
 
