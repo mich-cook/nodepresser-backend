28236414c74aa048cfd2f753fcb7be2ad849ae4b
mich <mich-cook@users.noreply.github.com>
2020-06-17 23:46:10 -0700

    hook up top authors API to the homepage that displays them

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-const HomePage = () => (
+const HomePage = () => {
+
+  const [ authors, setAuthors ] = useState({ "authors": [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`//localhost:8000/api/authors/top`);
+      const body = await result.json();
+      setAuthors(body);
+    };
+    fetchData();
+  }, []);
+
+return (
   <div id="homepage" className="page-body">
     <h1>Welcome to this blogging platform</h1>
     <div id="top-authors">
-      <p>Top Authors (TODO):</p>
+      <p>Top Authors:</p>
       <ul>
-        <li>Fake Author 1</li>
-        <li>Fake Author 2</li>
-        <li>Fake Author 3</li>
-        <li>Fake Author 4</li>
-        <li>Fake Author 5</li>
+      {authors.authors.map((author, key) => ( <li key={key}><Link to={`/articles/${author._id}`}>{author._id} ({author.count})</Link></li>))}
       </ul>
     </div>
     <div id="top-articles">
@@ -25,5 +35,7 @@ const HomePage = () => (
     </div>
   </div>
 );
+
+};
 
 export default HomePage;
